1b6546f2d178907f501d23eb5c562db4009374e4
Chan Nguyen <atbl1511@gmail.com>
2014-04-13 22:12:58 -0700

    Update string_util.h

--- RuberSystem/RuberSystem/string_util.h
+++ RuberSystem/RuberSystem/string_util.h
@@ -1,11 +1,3 @@
-/***************************************************************************
- * Class:		Comp 465 Fall 2012 - Computer Graphics Systems and Design  *
- * Instructor:	Dr. Mike Barnes										       *
- * Project:		Target Practice P3	                                       *
- * Due Date:	12/07/2012												   *
- * Students:    Chan Nguyen  - ngocchan.nguyen.61@my.csun.edu              *
- *				Lianhua Shen - lianhua.shen.124@my.csun.edu                *
- ***************************************************************************/
 #ifndef STRING_UTIL_H
 #define STRING_UTIL_H
 
